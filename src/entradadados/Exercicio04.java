d28a3dfb15d4a42b71f6be939c940e8802ccb3d8
Alex-junior644 <alexjuniorgs22@gmail.com>
2026-05-18 20:31:53 -0300

    commit inicial

--- src/entradadados/Exercicio04.java
+++ src/entradadados/Exercicio04.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Exercicio04 {
     public static void main(String[] args) {
 
+        
         Scanner entrada = new Scanner(System.in);
 
         // entrada de dados.
