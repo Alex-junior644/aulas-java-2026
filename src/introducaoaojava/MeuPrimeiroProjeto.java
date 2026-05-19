55a118c56b6a72cfb9b550ff948b18f64661ba21
Alex-junior644 <alexjuniorgs22@gmail.com>
2026-05-18 21:06:44 -0300

    Modificado arquivo meu primeiro projeto

--- src/introducaoaojava/MeuPrimeiroProjeto.java
+++ src/introducaoaojava/MeuPrimeiroProjeto.java
@@ -3,5 +3,6 @@ package introducaoaojava; //Nessa linha estar o pacote destar classe
 public class MeuPrimeiroProjeto { //aqui e unome da minha classe
     public static void main(String[] args) {
         System.out.println("Olá, mundo! Meu nome é Alex í Moro Em Feira De Santana!");
+        System.out.println("Fazendo um teste do git hub.");
     }
 }
